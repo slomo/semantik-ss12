0ec1bff0ab08b3ecf849c95a40f556f9ce3578e5
yves <steve.harrison@gmx.net>
2012-05-09 00:45:06 +0200

    almost working wskea semantic

--- 04/wskea.hs
+++ 04/wskea.hs
@@ -1,4 +1,5 @@
 import Control.Monad.State
+import qualified Control.Exception as E
 
 -- Semantik von Programmiersprachen
 -- AB3 Aufgabe 4
@@ -14,33 +15,33 @@ data COM = Skip
          | WhileDo BT COM
          | OutputBT BT
          | OutputT T
-         deriving (Show)
+         deriving (Show,Eq)
 data BT = Bool W
         | Not BT
         | BoolOp T BOP T
         | ReadBT
-        deriving (Show)
+        deriving (Show,Eq)
 data T = Num Z
        | ID Id
        | Op T OP T
        | ReadT
-       deriving (Show)
+       deriving (Show,Eq)
 data OP = Plus
         | Minus
         | Mult
         | Div
         | Mod
-        deriving (Show)
+        deriving (Show,Eq)
 data BOP = Equal
          | LessThan
          | GreaterThan
          | LessOrEqual
          | GreaterOrEqual
          | Unequal
-         deriving (Show)
+         deriving (Show,Eq)
 data KON = KONNum Z
          | KONBool W
-         deriving (Show)
+         deriving (Show,Eq)
 
 type Id = String
 type W = Bool
@@ -54,7 +55,7 @@ data K = KTerm T
        | KIf | KWhile | KNot | KOutput | KAssign | KPlus | KMinus | KMult
        | KDiv | KMod | KEqual | KLessThan | KGreaterThan | KLessOrEqual
        | KGreaterOrEqual | KUnequal
-       deriving (Show)
+       deriving (Show,Eq)
 
 --              W       S     K     E      A
 type WSKEAState = ([KON], Memory, [K], [KON], [KON])
@@ -84,167 +85,171 @@ assign id num memory
 anfang :: P -> [KON] -> WSKEAState
 anfang (Command c) input = ([], [], [KCommand c], input, [])
 
-delta :: WSKEAState -> WSKEAState
+delta :: WSKEAState -> Maybe WSKEAState
 -- 1.
 -- a)
-delta (w, s, (KTerm (Num z)):k, e, a) = ((KONNum z):w, s, k, e, a)
+delta (w, s, (KTerm (Num z)):k, e, a) = Just ((KONNum z):w, s, k, e, a)
 
 -- b)
 delta (w, s, (KTerm (ID id)):k, e, a) = case lookup id s of
-    Just z -> ((KONNum z):w, s, k, e, a)
-    Nothing -> error "undefined"
+    Just z -> Just $ ((KONNum z):w, s, k, e, a)
+    Nothing -> Nothing
 
 -- c) d) +
 delta (w, s, (KTerm (Op t1 Plus t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KPlus:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KPlus:k, e, a)
 
 delta ((KONNum n2):(KONNum n1):w, s, KPlus:k, e, a)
-    = ((KONNum (n1+n2)):w, s, k, e, a)
+    = Just ((KONNum (n1+n2)):w, s, k, e, a)
 
 -- e) -
 delta (w, s, (KTerm (Op t1 Minus t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KMinus:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KMinus:k, e, a)
 
 delta ((KONNum n2):(KONNum n1):w, s, KMinus:k, e, a)
-    = ((KONNum (n1-n2)):w, s, k, e, a)
+    = Just ((KONNum (n1-n2)):w, s, k, e, a)
 
 -- e) *
 delta (w, s, (KTerm (Op t1 Mult t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KMult:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KMult:k, e, a)
 
 delta ((KONNum n2):(KONNum n1):w, s, KMult:k, e, a)
-    = ((KONNum (n1*n2)):w, s, k, e, a)
+    = Just ((KONNum (n1*n2)):w, s, k, e, a)
 
 -- e) /
 delta (w, s, (KTerm (Op t1 Div t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KDiv:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KDiv:k, e, a)
 
 delta ((KONNum n2):(KONNum n1):w, s, KDiv:k, e, a)
-    = ((KONNum (n1 `div` n2)):w, s, k, e, a)
+    = Just ((KONNum (n1 `div` n2)):w, s, k, e, a)
 
 -- e) mod
 delta (w, s, (KTerm (Op t1 Mod t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KMod:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KMod:k, e, a)
 
 delta ((KONNum n2):(KONNum n1):w, s, KMod:k, e, a)
-    = ((KONNum (n1 `mod` n2)):w, s, k, e, a)
+    = Just ((KONNum (n1 `mod` n2)):w, s, k, e, a)
 
 -- f)
 delta (w, s, (KTerm ReadT):k, e1:e, a)
-    = (e1:w, s, k, e, a)
+    = Just (e1:w, s, k, e, a)
 
 -- 2.
 -- a)
 delta (w, s, (KBoolTerm (Bool b)):k, e, a)
-    = ((KONBool b):w, s, k, e, a)
+    = Just ((KONBool b):w, s, k, e, a)
 
 -- b) fehlt, da wir keine Bool-Werte speichern können
 -- c) d) Equal
 delta (w, s, (KBoolTerm (BoolOp t1 Equal t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KEqual:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KEqual:k, e, a)
 
 delta ((KONNum t2):(KONNum t1):w, s, KEqual:k, e, a)
-    = ((KONBool (t1 == t2)):w, s, k, e, a)
+    = Just ((KONBool (t1 == t2)):w, s, k, e, a)
 
 -- e) LessThan
 delta (w, s, (KBoolTerm (BoolOp t1 LessThan t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KLessThan:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KLessThan:k, e, a)
 
 delta ((KONNum t2):(KONNum t1):w, s, KLessThan:k, e, a)
-    = ((KONBool (t1 < t2)):w, s, k, e, a)
+    = Just ((KONBool (t1 < t2)):w, s, k, e, a)
 
 -- e) GreaterThan
 delta (w, s, (KBoolTerm (BoolOp t1 GreaterThan t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KGreaterThan:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KGreaterThan:k, e, a)
 
 delta ((KONNum t2):(KONNum t1):w, s, KGreaterThan:k, e, a)
-    = ((KONBool (t1 > t2)):w, s, k, e, a)
+    = Just ((KONBool (t1 > t2)):w, s, k, e, a)
 
 -- e) LessOrEqual
 delta (w, s, (KBoolTerm (BoolOp t1 LessOrEqual t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KLessOrEqual:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KLessOrEqual:k, e, a)
 
 delta ((KONNum t2):(KONNum t1):w, s, KLessOrEqual:k, e, a)
-    = ((KONBool (t1 <= t2)):w, s, k, e, a)
+    = Just ((KONBool (t1 <= t2)):w, s, k, e, a)
 
 -- e) GreaterOrEqual
 delta (w, s, (KBoolTerm (BoolOp t1 GreaterOrEqual t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KGreaterOrEqual:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KGreaterOrEqual:k, e, a)
 
 delta ((KONNum t2):(KONNum t1):w, s, KGreaterOrEqual:k, e, a)
-    = ((KONBool (t1 >= t2)):w, s, k, e, a)
+    = Just ((KONBool (t1 >= t2)):w, s, k, e, a)
 
 -- e) Unequal
 delta (w, s, (KBoolTerm (BoolOp t1 Unequal t2)):k, e, a)
-    = (w, s, (KTerm t1):(KTerm t2):KUnequal:k, e, a)
+    = Just (w, s, (KTerm t1):(KTerm t2):KUnequal:k, e, a)
 
 delta ((KONNum t2):(KONNum t1):w, s, KUnequal:k, e, a)
-    = ((KONBool (t1 /= t2)):w, s, k, e, a)
+    = Just ((KONBool (t1 /= t2)):w, s, k, e, a)
 
 -- f)
 delta (w, s, (KBoolTerm ReadBT):k, e1:e, a)
-    = (e1:w, s, k, e, a)
+    = Just (e1:w, s, k, e, a)
 
 -- 3.
 -- a)
 delta (w, s, (KCommand (Assign id t)):k, e, a)
-    = (w, s, (KTerm t):(KAssign):(KId id):k, e, a)
+    = Just (w, s, (KTerm t):(KAssign):(KId id):k, e, a)
 
 -- b)
 delta ((KONNum z):w, s, KAssign:(KId id):k, e, a)
-    = (w, assign id z s, k, e, a)
+    = Just (w, assign id z s, k, e, a)
 
 -- c)
 delta (w, s, (KCommand (c1 :.: c2)):k, e, a)
-    = (w, s, (KCommand c1):(KCommand c2):k, e, a)
+    = Just (w, s, (KCommand c1):(KCommand c2):k, e, a)
 
 -- d)
 delta (w, s, (KCommand (IfThenElse b c1 c2)):k, e, a)
-    = (w, s, (KBoolTerm b):KIf:(KCommand c1):(KCommand c2):k, e, a)
+    = Just (w, s, (KBoolTerm b):KIf:(KCommand c1):(KCommand c2):k, e, a)
 
 -- e)
 delta ((KONBool true):w, s, KIf:(KCommand c1):(KCommand c2):k, e, a)
-    = (w, s, (KCommand c1):k, e, a)
+    = Just (w, s, (KCommand c1):k, e, a)
 
 -- f)
 delta ((KONBool false):w, s, KIf:(KCommand c1):(KCommand c2):k, e, a)
-    = (w, s, (KCommand c2):k, e, a)
+    = Just (w, s, (KCommand c2):k, e, a)
 
 -- g)
 delta (w, s, (KCommand (WhileDo b c)):k, e, a)
-    = (w, s, (KBoolTerm b):KWhile:(KBoolTerm b):(KCommand c):k, e, a)
+    = Just (w, s, (KBoolTerm b):KWhile:(KBoolTerm b):(KCommand c):k, e, a)
 
 -- h)
 delta ((KONBool True):w, s, KWhile:(KBoolTerm b):(KCommand c):k, e, a)
-    = (w, s, (KCommand c):(KBoolTerm b):KWhile:(KBoolTerm b):(KCommand c):k, e, a)
+    = Just (w, s, (KCommand c):(KBoolTerm b):KWhile:(KBoolTerm b):(KCommand c):k, e, a)
 
 -- i)
 delta ((KONBool False):w, s, KWhile:(KBoolTerm b):(KCommand c):k, e, a)
-    = (w, s, k, e, a)
+    = Just (w, s, k, e, a)
 
 -- j)
 delta (w, s, (KCommand (OutputT t)):k, e, a)
-    = (w, s, (KTerm t):KOutput:k, e, a)
+    = Just (w, s, (KTerm t):KOutput:k, e, a)
 
 -- k)
 delta ((KONNum z):w, s, KOutput:k, e, a)
-    = (w, s, k, e, a++[(KONNum z)])
+    = Just (w, s, k, e, a++[(KONNum z)])
 
 -- j) k) für Bool
 delta (w, s, (KCommand (OutputBT bt)):k, e, a)
-    = (w, s, (KBoolTerm bt):KOutput:k, e, a)
+    = Just (w, s, (KBoolTerm bt):KOutput:k, e, a)
 
 delta ((KONBool b):w, s, KOutput:k, e, a)
-    = (w, s, k, e, a++[(KONBool b)])
+    = Just (w, s, k, e, a++[(KONBool b)])
 
-delta _ = error "undefined state"
+delta _ = Nothing
 
--- Testaufruf
-divprogtest = (putStr . addLinebreaks . showWSKEAState . delta . delta . delta . delta . 
-    delta . delta . delta . delta . delta . delta . delta . delta . delta . delta .
-    delta . delta . delta . delta . delta . delta . delta . delta . delta . delta .
-    delta . delta . delta . delta . delta . delta . delta . delta . delta . delta .
-    delta . delta . delta . delta . delta . delta) (anfang divprog [KONNum 1, KONNum 1])
+-- divprog
+divprog :: P
+divprog = Command
+          (((Assign "x" (ReadT)):.:
+            (Assign "y" (ReadT))):.:
+           ((Assign "g" (Num 0)):.:                            -- g := 0;
+            (WhileDo (BoolOp (ID "x") GreaterOrEqual (ID "y")) -- while (x >= y)
+                (Assign "x" (Op (ID "x") Minus (ID "y")):.:    --     x = x - y;
+                 Assign "g" (Op (ID "g") Plus (Num 1))))):.:   --     g = g + 1;
+            (OutputT (ID "g")))                                -- output g
 
 -- Übung 4, Aufgabe 1
 o :: P -> [KON] -> [KON]
@@ -253,10 +258,20 @@ o code input = evalState o' (anfang code input)
 o' :: State WSKEAState [KON]
 o' = do
 	oldState <- get
-	modify delta
-	newState <- get
-	if oldState == newState
-		then return (output newState)
-		else o'
+	let maybeNewState = delta oldState
+	case maybeNewState of
+		Just someState ->
+			if someState == oldState
+			then return $ output oldState
+			else do
+				put someState
+				o'
+		Nothing	-> return []
 
 output (w, s, k, e, a) = a
+values (w, s, k, e, a) = w
+
+
+main :: IO ()
+main =
+    putStr $ show (o divprog [KONNum 1, KONNum 2])
